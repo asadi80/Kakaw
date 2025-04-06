460a5ab087eedbf400e1cba29d0605e543a2e4a7
Abdurraouf <a.k.sadi80@gmail.com>
2025-04-06 16:12:51 -0700

    update loader

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -8,7 +8,6 @@ import { useQRCode } from "next-qrcode";
 import AddLink from "../../components/addLink";
 import AboutMe from "../../components/aboutMe";
 
-
 function Profile() {
   const router = useRouter();
   const { Image } = useQRCode();
@@ -49,7 +48,7 @@ function Profile() {
           setUser(data);
           setLinks(data.links);
           setImageUrl(data.profilePicture);
-          setAboutMe(data.aboutMe)
+          setAboutMe(data.aboutMe);
           setFormData({
             name: data.name || "",
             email: data.email || "",
@@ -200,7 +199,28 @@ function Profile() {
     router.push(`/user/${id}`);
   };
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          class="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span class="sr-only">Loading...</span>
+      </div>
+    );
 
   return (
     <div className=" max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
@@ -407,7 +427,7 @@ function Profile() {
       {/* -------------------------------about me-------------------------------------------------------------- */}
       <div className="p-4 border-t mx-8 mt-2">
         <div className="mt-2 space-y-2">
-          <AboutMe  aboutMe={aboutMe} setAboutMe={setAboutMe}/>
+          <AboutMe aboutMe={aboutMe} setAboutMe={setAboutMe} />
         </div>
       </div>
 
@@ -427,15 +447,14 @@ function Profile() {
                 className="flex items-center justify-center w-full "
               >
                 <button className="bg-white w-full text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                    <a
-                      key={link._id}
-                      href={link.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {link.title}
-                    </a>
-                 
+                  <a
+                    key={link._id}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {link.title}
+                  </a>
                 </button>
                 <button onClick={() => handelDeleteLink(link._id)}>
                   <svg
